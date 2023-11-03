95eb2ccbb1d242dec65c2d121fb3f3ef5d21207f
Mohsen <ahmadi.mohsen1714@gmail.com>
2023-11-03 23:51:09 +0200

    initial commit

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+const Heroes = () => {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+          <Image
+            className="object-contain dark:hidden"
+            src="/documents.png"
+            fill
+            alt="Documents"
+          />
+          <Image
+            className="object-contain hidden dark:block"
+            src="/documents-dark.png"
+            fill
+            alt="Documents"
+          />
+        </div>
+        <div className="relative h-[400px] w-[400px] hidden md:block ">
+          <Image
+            src="/reading.png"
+            fill
+            alt="Reading"
+            className="object-contain dark:hidden "
+          />
+          <Image
+            src="/reading-dark.png"
+            fill
+            alt="Reading"
+            className="object-contain hidden dark:block "
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Heroes;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://mighty-dodo-89.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
